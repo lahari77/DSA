c382f53041190c3b1ae94c85bb1a195d02747f63
Pedineedi Lahari <laharipedineedi@gmail.com>
2022-07-03 16:54:45 +0530

    1D demo files added

---
+++ Day13/array1DimDemo2.py
@@ -0,0 +1,20 @@
+from array import *
+arr1 = array('i', [1,2,3,4,5,6])
+def searchElement(array, value):
+    for i in array:
+        if i == value:
+            return array.index(i)
+    return "The element not found in array"
+
+
+print(searchElement(arr1, 3))
+print(searchElement(arr1, 6))
+print(searchElement(arr1, 7))
+
+arr2 = [1,2,3,4,5,6]
+arr2.remove(1)
+print(arr2)
+arr2.remove(6)
+print(arr2)
+arr2.remove(4)
+print(arr2)
