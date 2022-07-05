889a05bfd78b38e6ae91acd5b9bc6993df23b16c
Pedineedi Lahari <laharipedineedi@gmail.com>
2022-07-05 23:19:57 +0530

    added new code

--- Day15/Listsdemo2.py
+++ Day15/Listsdemo2.py
@@ -14,3 +14,39 @@ def searchinList(list, value):
 
 print(searchinList(myList, 20))
 print(searchinList(myList, 100))
+
+# List operations/functions
+a = [1, 2, 3]
+b = [4, 5]
+c = a + b
+print(c)
+a = [0]
+print(a*4)
+a = [0, 1]
+print(a*4)
+a = [0, 1, 2, 3, 4, 5, 6]
+print(len(a))
+print(max(a))
+print(min(a))
+print(sum(a)/len(a))
+
+# Test covert below using functions
+total = 0
+count = 0
+while(True):
+    inp = input("Enter a value: ")
+    if inp == 'done': break
+    value = float(inp)
+    total += value
+    count += 1
+    average = total/count
+print('Average: ', average)
+
+myList = []
+while(True):
+    inp = input("Enter a value: ")
+    if inp == 'done': break
+    myList.append(float(inp))
+    average = sum(myList)/len(myList)
+print('Average: ', average)
+
